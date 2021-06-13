dd0dd1438fa28583893634dc7d66b071c517f472
akshat <akshatbhargavadeepa@gmail.com>
2021-06-13 20:17:36 +0530

    feat: added bottom section

--- components/Home/index.js
+++ components/Home/index.js
@@ -174,7 +174,38 @@ function Home() {
                             <h4 className="text-xl font-medium text-gray-700">Coupons</h4>
                             <p className="text-base text-center text-gray-500">Coupons system to provide special offers and discounts for your app.</p>
                         </div> */}
+                    </div>
+                </div>
+            </section>
 
+            <section className="py-8 leading-7 text-gray-900 bg-white sm:py-12 md:py-16 flex">
+                <div className="container max-w-6xl mx-auto lg:py-24 px-3">
+                    <h2 className="text-4xl font-bold tracking-tight text-center">Backed by top providers</h2>
+                    <p className="mt-2 text-lg text-center text-gray-600">We support one-click deployment to any of these providers</p>
+                    <div className="flex justify-center"><img className="sm:w-1/2" src="https://miro.medium.com/max/3222/1*go7sTFOGN2fJGgYrI3E-FA.png" alt="" /></div>
+                </div>
+            </section>
+
+            <div className="border-b border-gray-300"></div>
+
+            <section className="py-8 leading-7 text-gray-900 bg-white sm:py-12 md:py-16 lg:py-24">
+                <div className="max-w-6xl px-4 px-10 mx-auto border-solid lg:px-12">
+                    <div className="flex flex-col items-start leading-7 text-gray-900 border-0 border-gray-200 lg:items-center lg:flex-row">
+                        <div className="box-border flex-1 text-center border-solid sm:text-left">
+                            <h2 className="m-0 text-4xl font-semibold leading-tight tracking-tight text-left text-black border-0 border-gray-200 sm:text-5xl">
+                                Leave the hard stuff on us
+                            </h2>
+                            <p className="mt-2 text-xl text-left text-gray-900 border-0 border-gray-200 sm:text-2xl">
+                                Start your first blockchain node now
+                            </p>
+                        </div>
+                        <a href="#_" className="inline-flex items-center justify-center w-full px-5 py-4 mt-6 ml-0 font-sans text-base leading-none text-white no-underline bg-red-500 border border-red-500 border-solid rounded cursor-pointer md:w-auto lg:mt-0 hover:bg-red-600 hover:border-red-600 hover:text-white focus-within:bg-red-600 focus-within:border-red-600 focus-within:text-white sm:text-lg lg:ml-6 md:text-xl">
+                            Get Started
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
+                                <line x1="5" y1="12" x2="19" y2="12"></line>
+                                <polyline points="12 5 19 12 12 19"></polyline>
+                            </svg>
+                        </a>
                     </div>
                 </div>
             </section>
