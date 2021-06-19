ef09510a7d846b9508a6e6edb41bd19d3983eb86
akshat <akshatbhargavadeepa@gmail.com>
2021-06-19 17:13:02 +0530

    feat: added cloud-providers constant file

---
+++ constants/cloud-providers.js
@@ -0,0 +1,17 @@
+export const CloudProviders = [
+    {
+        id: 0,
+        name: 'AWS',
+        imageSrc: '/images/aws-logo-min.png',
+    },
+    {
+        id: 1,
+        name: 'GCP',
+        imageSrc: '/images/gcp-logo-min.png',
+    },
+    {
+        id: 0,
+        name: 'MA',
+        imageSrc: '/images/azure-logo-min.png',
+    },
+];
