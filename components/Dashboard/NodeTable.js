275ab55b259190acfc98aed39917b52d516e2bc7
akshat <akshatbhargavadeepa@gmail.com>
2021-06-19 21:53:12 +0530

    feat: data integrated in node-table component

--- components/Dashboard/NodeTable.js
+++ components/Dashboard/NodeTable.js
@@ -1,4 +1,8 @@
-function NodeTable() {
+import { Tooltip } from '@chakra-ui/react';
+import { CloudProviders } from "@constants/cloud-providers";
+import { NodeTypes } from "@constants/node-types";
+
+function NodeTable({ nodes }) {
     return (
         <div className="bg-white shadow-md rounded my-6">
             <table className="w-full table-auto">
@@ -12,47 +16,49 @@ function NodeTable() {
                     </tr>
                 </thead>
                 <tbody className="text-gray-600 text-sm font-light">
-                    <tr className="border-b border-gray-200 hover:bg-gray-100">
-                        <td className="py-3 px-6 text-left whitespace-nowrap">
-                            <div className="flex items-center">
-                                <span className="font-medium">Main Staking Node</span>
-                            </div>
-                        </td>
-                        <td className="py-3 px-6 text-left">
-                            <div className="flex items-center">
-                                <div className="mr-2">
-                                    <img className="w-6 h-6 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/BTC_Logo.svg/183px-BTC_Logo.svg.png" />
+                    {nodes.map(node => (
+                        <tr key={node._id} className="border-b border-gray-200 hover:bg-gray-100">
+                            <td className="py-3 px-6 text-left whitespace-nowrap">
+                                <div className="flex items-center">
+                                    <span className="font-medium">{node.name}</span>
                                 </div>
-                                <span>Bitcoin</span>
-                            </div>
-                        </td>
-                        <td className="py-3 px-6 text-center">
-                            AWS - us-central-1
-                        </td>
-                        <td className="py-3 px-6 text-center">
-                            <span className="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Active</span>
-                        </td>
-                        <td className="py-3 px-6 text-center">
-                            <div className="flex item-center justify-center">
-                                <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
+                            </td>
+                            <td className="py-3 px-6 text-left">
+                                <div className="flex items-center">
+                                    <div className="mr-2">
+                                        <img className="w-6 h-6 rounded-full" src={NodeTypes[node.type].imageSrc} />
+                                    </div>
+                                    <span>{NodeTypes[node.type].name}</span>
                                 </div>
-                                <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
+                            </td>
+                            <td className="py-3">
+                                <div className="flex justify-center">
+                                    <img width={60} height={60} src={CloudProviders[node.datacenter].imageSrc} />
                                 </div>
-                                <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
+                            </td>
+                            <td className="py-3 px-6 text-center">
+                                <span className="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Active</span>
+                            </td>
+                            <td className="py-3 px-6 text-center">
+                                <div className="flex item-center justify-center space-x-4">
+                                    <Tooltip hasArrow label="Monitor Node">
+                                        <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                                            </svg>
+                                        </div>
+                                    </Tooltip>
+                                    <Tooltip hasArrow label="Delete Node">
+                                        <div className="w-4 mr-2 transform hover:text-red-500 hover:scale-110 cursor-pointer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg>
+                                        </div>
+                                    </Tooltip>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
