8af04f3ba851db9393720c980117b5a988c303db
akshat <akshatbhargavadeepa@gmail.com>
2021-06-19 10:47:40 +0530

    feat: added create-node button

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -5,9 +5,8 @@ import firebase from 'firebase/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/dist/client/router';
 
-function Header() {
+function Header({ user, loading }) {
     const router = useRouter();
-    const [user, loading, error] = useAuthState(firebase.auth());
 
     useEffect(() => {
         if (!user && !loading) router.replace('/');
@@ -24,11 +23,25 @@ function Header() {
 }
 
 function Dashboard() {
+    const [user, authLoading, error] = useAuthState(firebase.auth());
+
     return (
         <div>
-            <Header />
+            <Header user={user} loading={authLoading} />
             <div className="w-full mx-auto m-10 max-w-4xl">
-                <h1 className="text-red-500">Welcome to dashboard</h1>
+                <div className="flex items-start justify-end">
+                    {/* <h1 className="font-semibold"></h1> */}
+                    <button className="bg-red-500 hover:bg-opacity-80 text-white px-4 py-2 rounded-lg font-semibold flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
+                        </svg>
+                        <p className="ml-2">Create Node</p>
+                    </button>
+                </div>
+
+                <div>
+                    
+                </div>
             </div>
         </div>
     )
