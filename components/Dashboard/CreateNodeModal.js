8d0ee4e6d75f9f28df8b6b35706aa6174ea846aa
akshat <akshatbhargavadeepa@gmail.com>
2021-06-19 21:37:06 +0530

    feat: created a "done" state in create-node-modal

--- components/Dashboard/CreateNodeModal.js
+++ components/Dashboard/CreateNodeModal.js
@@ -1,5 +1,6 @@
 import {
     Modal,
+    ModalCloseButton,
     ModalOverlay,
     ModalContent,
     ModalHeader,
@@ -91,7 +92,13 @@ function CreateNodeModal({ isOpen, onClose, onCreateNode }) {
     const [name, setName] = useState('');
     const [nodeType, setNodeType] = useState();
     const [provider, setProvider] = useState();
+    const [submitting, setSubmitting] = useState(false);
+    const [done, setDone] = useState(true);
     
+    useEffect(() => {
+        if (isOpen) setDone(false);
+    }, [isOpen]);
+
     const shouldDisableCTA = () => {
         switch (step.label) {
             case Steps.NODE_NAME.label:
@@ -103,22 +110,40 @@ function CreateNodeModal({ isOpen, onClose, onCreateNode }) {
         }
     };
 
+    const onSubmit = () => {
+        setSubmitting(true);
+        onCreateNode(name, nodeType, provider).finally(() => {
+            setTimeout(() => {
+                setSubmitting(false);
+
+                // reset state
+                setStep(Steps.NODE_NAME);
+                setName('');
+                setNodeType();
+                setProvider();
+                setDone(true);
+            }, 1500);
+
+        });
+    };
+
     return (
         <Modal motionPreset="slideInBottom" size="2xl" isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
+                {done && <ModalCloseButton />}
                 <ModalHeader>
-                    <span className="text-2xl text-red-500 font-extrabold">Instantiate a new Node</span>
+                    <span hidden={done} className="text-2xl text-red-500 font-extrabold">Instantiate a new Node</span>
                 </ModalHeader>
                 <ModalBody>
-                    {step.label === Steps.NODE_NAME.label && (
+                    {!done && step.label === Steps.NODE_NAME.label && (
                         <div>
                             <h3 className="mb-4 text-lg text-gray-800 font-semibold">Give your node a name (or nickname if you'd like)</h3>
                             <Input width="96" fontWeight="semibold" value={name} onChange={ev => setName(ev.target.value)} />
                         </div>
                     )}
 
-                    {step.label === Steps.PROVIDER_SELECTION.label && (
+                    {!done && step.label === Steps.PROVIDER_SELECTION.label && (
                         <div>
                             <h3 className="text-lg text-gray-800 font-semibold flex items-center">
                                 <span className="mr-2">Choose the service provider</span>
@@ -135,7 +160,7 @@ function CreateNodeModal({ isOpen, onClose, onCreateNode }) {
                         </div>
                     )}
 
-                    {step.label === Steps.NODE_TYPE.label && (
+                    {!done && step.label === Steps.NODE_TYPE.label && (
                         <div>
                             <h3 className="text-lg text-gray-800 font-semibold flex items-center">
                                 <span className="mr-2">Which type of node would you like to deploy?</span>
@@ -145,10 +170,18 @@ function CreateNodeModal({ isOpen, onClose, onCreateNode }) {
                             </div>
                         </div>
                     )}
+
+                    {done && (
+                        <div>
+                            <h3 className="text-4xl text-black font-extrabold">And done, your node is created<span className="text-red-500">.</span></h3>
+                            <h5 className="ml-1 mt-2 text-lg text-gray-400">how easy was that? 🥳</h5>
+                            <img src="/images/node-created.png" alt="Node Created" />
+                        </div>
+                    )}
                 </ModalBody>
 
                 <ModalFooter>
-                    <div className="w-full flex items-center justify-between">
+                    <div hidden={done} className="w-full flex items-center justify-between">
                         <Button
                             variant="outline"
                             disabled={!step.prev}
@@ -163,9 +196,10 @@ function CreateNodeModal({ isOpen, onClose, onCreateNode }) {
                             mr={3}
                             colorScheme="red"
                             disabled={shouldDisableCTA()}
-                            onClick={() => step.next ? setStep(Steps[step.next]) : onCreateNode(name, nodeType, provider)}
+                            onClick={() => step.next ? setStep(Steps[step.next]) : onSubmit()}
                             transition="all"
                             transitionDuration={500}
+                            isLoading={submitting}
                         >
                             {step.next ? 'Next' : 'Finish'}
                             <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
