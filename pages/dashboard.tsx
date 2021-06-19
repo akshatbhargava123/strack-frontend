5ea737dbff49b237a97d193c4ac19831585fc9e7
akshat <akshatbhargavadeepa@gmail.com>
2021-06-19 22:11:01 +0530

    feat: checking isMobile for dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import '@lib/init-firebase';
+import Link from 'next/link';
 import Head from 'next/head';
 import Dashboard from '@components/Dashboard';
+import { isMobile } from 'react-device-detect';
 
 export default function HomePage() {
   return (
@@ -11,7 +13,14 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Dashboard />
+      {isMobile ? (
+        <div className="min-h-screen flex flex-col justify-center px-5">
+          <h3 className="text-black font-semibold text-2xl">Sorry, we don't support this screen size yet for dashboard.</h3>
+          <p className="text-gray-600">Try opening on your computer's browser</p>
+        </div>
+      ) : (
+        <Dashboard />
+      )}
 
     </div>
   )
