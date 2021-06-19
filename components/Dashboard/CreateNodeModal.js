2fb4e80ba8c53e51a236893428f58ae9f1404f72
akshat <akshatbhargavadeepa@gmail.com>
2021-06-19 17:37:22 +0530

    feat: created platform-selector component

--- components/Dashboard/CreateNodeModal.js
+++ components/Dashboard/CreateNodeModal.js
@@ -7,16 +7,41 @@ import {
     ModalBody,
     Button,
     Input,
+    Tooltip,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CloudProviders } from '@constants/cloud-providers';
+
+function PlatformSelector({ setProvider }) {
+    const [selected, setSelected] = useState();
+
+    useEffect(() => {
+        setProvider(selected);
+    }, [selected]);
+
+    return (
+        <div className="flex flex-wrap space-x-2">
+            {CloudProviders.map(provider => (
+                <div
+                    key={provider.id}
+                    className={`p-2 transition-all duration-200 shadow-lg cursor-pointer rounded ${selected === provider.id ? 'ring-2 ring-red-300 bg-red-50' : 'ring-1 ring-gray-200 opacity-80'}`}
+                    onClick={() => setSelected(provider.id)}
+                >
+                    <img width={180} height={180} src={provider.imageSrc} alt="aws" />
+                </div>
+            ))}
+        </div>
+    )
+}
 
 function CreateNodeModal({ isOpen, onClose }) {
     const [step, setStep] = useState(1);
     const [name, setName] = useState('');
+    const [provider, setProvider] = useState();
     
     const shouldDisableCTA = () => {
         if (step === 1) return !!!name.trim().length;
-        // if (step === 2) return 
+        if (step === 2) return isNaN(Number(provider));
     };
 
     const updateStep = (delta) => {
@@ -40,8 +65,17 @@ function CreateNodeModal({ isOpen, onClose }) {
 
                     {step === 2 && (
                         <div>
-                            <h3 className="mb-4 text-lg text-gray-800 font-semibold">Choose the service provider</h3>
-                            <Input width="96" fontWeight="semibold" />
+                            <h3 className="text-lg text-gray-800 font-semibold flex items-center">
+                                <span className="mr-2">Choose the service provider</span>
+                                <Tooltip placement="right" hasArrow label="Feel free to choose any if you're a beginner as we've integrated all of these with best class security" fontSize="md">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+                                    </svg>
+                                </Tooltip>
+                            </h3>
+                            <div className="py-8">
+                                <PlatformSelector setProvider={setProvider} />
+                            </div>
                         </div>
                     )}
 
@@ -60,13 +94,16 @@ function CreateNodeModal({ isOpen, onClose }) {
                             disabled={step === 1}
                             onClick={() => updateStep(-1)}
                         >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+                            </svg>
                             Previous
                         </Button>
                         <Button
                             mr={3}
                             colorScheme="red"
-                            onClick={onClose}
                             disabled={shouldDisableCTA()}
+                            onClick={() => updateStep(1)}
                         >
                             Next
                             <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
