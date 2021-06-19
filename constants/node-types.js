ac7661146b56c2358bd7185e88ab85dc3582b123
akshat <akshatbhargavadeepa@gmail.com>
2021-06-19 20:19:11 +0530

    feat: added node-types constant file

---
+++ constants/node-types.js
@@ -0,0 +1,17 @@
+export const NodeTypes = [
+    {
+        id: 0,
+        name: 'Bitcoin',
+        imageSrc: '/images/bitcoin-logo-min.png',
+    },
+    {
+        id: 1,
+        name: 'Elrond',
+        imageSrc: '/images/elrond-logo-min.png',
+    },
+    {
+        id: 2,
+        name: 'Ethereum',
+        imageSrc: '/images/ethereum-logo-min.png',
+    },
+];
