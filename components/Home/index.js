3580deed76c44ab3b2d59393d0e3752697a10620
akshat <akshatbhargavadeepa@gmail.com>
2021-06-20 16:26:10 +0530

    feat: Get started button on home now initiates login

--- components/Home/index.js
+++ components/Home/index.js
@@ -247,7 +247,7 @@ function Home() {
                                 Start your first blockchain node now
                             </p>
                         </div>
-                        <a href="#_" className="inline-flex items-center justify-center w-full px-5 py-4 mt-6 ml-0 font-sans text-base leading-none text-white no-underline bg-red-500 border border-red-500 border-solid rounded cursor-pointer md:w-auto lg:mt-0 hover:bg-red-600 hover:border-red-600 hover:text-white focus-within:bg-red-600 focus-within:border-red-600 focus-within:text-white sm:text-lg lg:ml-6 md:text-xl">
+                        <a onClick={signInWithGoogle} className="inline-flex items-center justify-center w-full px-5 py-4 mt-6 ml-0 font-sans text-base leading-none text-white no-underline bg-red-500 border border-red-500 border-solid rounded cursor-pointer md:w-auto lg:mt-0 hover:bg-red-600 hover:border-red-600 hover:text-white focus-within:bg-red-600 focus-within:border-red-600 focus-within:text-white sm:text-lg lg:ml-6 md:text-xl">
                             Get Started
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
                                 <line x1="5" y1="12" x2="19" y2="12"></line>
